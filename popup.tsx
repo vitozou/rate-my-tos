0681ba5c14edeacd1e937179221241237c44c0c2
notrichardpeng <rpeng@worldcubeassociation.org>
2024-11-09 22:39:38 -0800

    more popup changes

--- popup.tsx
+++ popup.tsx
@@ -5,28 +5,54 @@ import splashart from "./assets/splashart.png";
 import "./popup.css";
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const API_HOST = "http://127.0.0.1:8000/api";
 
   const [currentURL, setCurrentURL] = useState("");
+  const [data, setData] = useState("");
 
-  useEffect(
-    () =>
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        function (tabs) {
-          const tab = tabs[0];
-          if (tab.url) {
-            setCurrentURL(tab.url);
+  useEffect(() => {
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      function (tabs) {
+        const tab = tabs[0];
+        if (tab.url) {
+          setCurrentURL(tab.url);
+        }
+      },
+    );
+  }, [chrome]);
+
+  useEffect(() => {
+    const scrapeTOS = async () => {
+      if(currentURL){
+        try {
+          const response = await fetch(`${API_HOST}/run-script/`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ url: currentURL }),
+          });
+    
+          const data = await response.json();
+          console.log(data);
+
+          if (response.ok) {
+            setData(data["response"]);
+          } else {
+            console.error("Error:", data.error);
           }
-        },
-      ),
-    [chrome],
-  );
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      }
+    };
 
-  console.log(currentURL);
+    scrapeTOS();
+  }, [currentURL]);  
 
   return (
     <>
